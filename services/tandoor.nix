54923e73bc7a91082a88cfae9abf573f2b8c807e
Scharada <scharada+github@marmels.de>
2025-02-04 20:06:02 +0100

    try out

--- services/tandoor.nix
+++ services/tandoor.nix
@@ -8,8 +8,8 @@
   services.tandoor-recipes = {
     enable = true;
     #extraConfig = {};
-    port = 28982;
+    #port = 28982;
     #address = "localhost";
-    address = "tandoor.local";
+    #address = "tandoor.local";
   };
 }
